1fed74e883763b9a944db20127c09c314e583998
samminhch@wsl <45446622+samminhch@users.noreply.github.com>
2023-05-11 12:31:59 -0500

    added flex layout and got rid of white padding on page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,14 @@
     const profile_image = { src: './src/lib/profile_pic.jpg', alt: 'Minh Nguyen' }
 </script>
 
-<Card title='Minh Nguyen' {...profile_image} />
-<Card title='About Me' text='Hello, world!'/>
+<div class="flex-container">
+    <Card title="Minh Nguyen" {...profile_image} />
+    <Card title="About Me" text="Hello, world! I'm an incoming freshman at the University of Texas at Dallas that's majoring in computer engineering!" />
+</div>
+
+<style lang="scss">
+    .flex-container {
+        display: flex;
+        gap: 1em;
+    }
+</style>
